12e3f74659e879d675c2aab7180c8268651aa9dd
Jonathan Phipps <jonathanmp25@gmail.com>
2018-08-06 12:53:45 -0500

    Add chart-js-2

--- src/components/homePage/dateElement/dateWordElement/wordDataPanel/index.js
+++ src/components/homePage/dateElement/dateWordElement/wordDataPanel/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Bar } from 'react-chartjs-2';
 
-const WordDataPanel = ({ word }) => (
-  <div>
-    { word.count }
-  </div>
-)
+class WordDataPanel extends React.Component {
+  data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }
+    ]
+  };
+  componentDidMount() {
+
+  }
+  render() {
+    return (
+      <div>
+        <Bar data={this.data}/>
+      </div>
+    )
+  }
+}
 
 export default WordDataPanel;
