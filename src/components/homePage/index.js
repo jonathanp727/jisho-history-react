9409f1dda6f0813bcd7dd211acd5b532a2f48a93
Jonathan Phipps <jonathanmp25@gmail.com>
2018-08-21 17:48:07 -0500

    Add empty page warnings

--- src/components/homePage/index.js
+++ src/components/homePage/index.js
@@ -8,6 +8,14 @@ const HomePageComponent = ({ words }) => (
   <div className={styles.homePage}>
     {
       (() => {
+        if(words.length === 0) {
+          return (
+            <div className={styles.emptyCont}>
+              <h2 className={styles.emptyHeader}>There's nothing here!</h2>
+              <p className={styles.emptyDesc}>Add words with the Jisho History Chrome Extension by clicking the "+" button below any entry on jisho.org</p>
+            </div>
+          )
+        }
         let curDate = new Date(0);
         return words.map(word => {
           let date = new Date(word.latestIncrement);
